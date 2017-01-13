963cef300fd83f6e285daec1fd9934a715316bba
Sonu Abraham <sonuabraham2001@gmail.com>
2017-01-14 09:39:10 +1100

    second commit

---
+++ Test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+	public void pupAge(){
+		//int age= 0;
+		int age;
+		age = age + 7;
+		System.out.println("Puppy age is " + age);
+	}
+
+
+	public static void main(String [] args)
+	{
+		System.out.println("Hello Test Program");
+		Test test = new Test();
+		test.pupAge();
+
+	}
+}
